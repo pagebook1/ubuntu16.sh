63d13764d8ddd3356245bec292439d6e6fb8aa30
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2021-04-26 22:30:23 +0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#code sudo wget https://www.dropbox.com/s/mkddaagdk2vxcvu/script.sh && chmod +x script.sh && bash ./script.sh
+sudo wget https://raw.githubusercontent.com/pagebook1/ubuntu16.sh/main/ubuntu16.sh && chmod +x ubuntu16.sh && bash ./ubuntu16.sh
