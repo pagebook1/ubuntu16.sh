0007866d7a211afa32ed1824a99c56fc80c9a7c1
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2021-04-27 13:13:48 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-#code sudo wget https://www.dropbox.com/s/mkddaagdk2vxcvu/script.sh && chmod +x script.sh && bash ./script.sh
+#code sudo wget https://raw.githubusercontent.com/pagebook1/ubuntu16.sh/main/script.sh && chmod +x script.sh && bash ./script.sh
+echo Enter License Key: 
+read license
+if [ $license == 'kevinbeetle' ]
+then
+echo   SERVER SCRIPT INSTALLING 
+else
+    echo Invalid License Key.
+    exit
+fi
 sudo wget https://raw.githubusercontent.com/pagebook1/ubuntu16.sh/main/ubuntu16.sh && chmod +x ubuntu16.sh && bash ./ubuntu16.sh
